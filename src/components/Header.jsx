c02dd8e6ffce2ebf5ac65db4b8689adbd5078b4c
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-11-30 06:07:36 +0200

    removed template favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="header">
+      <h4>I'm the header</h4>
       {/* Logo, SearchBar, FavoritesButton go here */}
     </header>
   );
