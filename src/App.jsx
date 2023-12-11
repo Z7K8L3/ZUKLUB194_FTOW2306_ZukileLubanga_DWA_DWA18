1b2fd10c045326f07f183054dc5bac55d289ecd1
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-12-11 13:16:05 +0200

    Added a favorites button and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SignUp, Login, Home } from "./auth";
 import { Routes, Route } from "react-router-dom";
+import Favorites from "./components/Favorites";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Route path={"/signup"} element={<SignUp />} />
         <Route path={"/"} element={<Login />} />
         <Route path={"/homepage"} element={<Home />} />
+        <Route path={"/favorites"} element={<Favorites />} />
       </Routes>
     </div>
   );
---
+++ src/components/Favorites.jsx
@@ -0,0 +1,10 @@
+// Favorites.jsx
+import React, { useState, useEffect } from "react";
+
+export default function Favorites() {
+  return (
+    <div>
+        <h2>My Favorites</h2>
+    </div>
+  );
+}
