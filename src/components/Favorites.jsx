c3cbceffcac978709c65fbdf6728cd07ccf30aef
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-12-11 15:14:31 +0200

    created a supabase table for storage

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,53 +1,74 @@
 import React, { useState, useEffect } from "react";
+import { supabase } from "../client";
 
 export default function Favorites() {
-  const [favorites, setFavorites] = useState([]); // You'll fetch and set favorites later
-  const [sortBy, setSortBy] = useState("dateDesc"); // Default sorting by date in descending order
+  const [favorites, setFavorites] = useState([]);
+  const [sortBy, setSortBy] = useState("dateDesc");
 
-  // useEffect for fetching favorites (to be added later)
   useEffect(() => {
-    // Fetch and set favorites here
+    const fetchFavorites = async () => {
+      try {
+        const { data, error } = await supabase.from("favorites").select("*");
+        if (error) {
+          throw error;
+        }
+        setFavorites(data || []);
+      } catch (error) {
+        console.error("Error fetching favorites:", error.message);
+      }
+    };
+
+    fetchFavorites();
   }, []);
 
   const sortFavorites = () => {
-    // Implement sorting logic based on the current sortBy state
-    // You may need to modify this based on your actual data structure
-    let sortedFavorites = [...favorites];
+    // Implement sorting logic
 
-    switch (sortBy) {
-      case "titleAsc":
-        sortedFavorites.sort((a, b) => a.showTitle.localeCompare(b.showTitle));
-        break;
-      case "titleDesc":
-        sortedFavorites.sort((a, b) => b.showTitle.localeCompare(a.showTitle));
-        break;
-      case "dateAsc":
-        sortedFavorites.sort((a, b) => new Date(a.dateAdded) - new Date(b.dateAdded));
-        break;
-      case "dateDesc":
-        sortedFavorites.sort((a, b) => new Date(b.dateAdded) - new Date(a.dateAdded));
-        break;
-      default:
-        break;
-    }
-
-    setFavorites(sortedFavorites);
+    // Update favorites in Supabase after sorting
+    updateFavorites(sortedFavorites);
   };
 
-  return (
-    <div>
-      <h2>My Favorites</h2>
-      
-      {/* Sorting buttons/menus */}
-      <button onClick={() => setSortBy("titleAsc")}>Sort by Title A-Z</button>
-      <button onClick={() => setSortBy("titleDesc")}>Sort by Title Z-A</button>
-      <button onClick={() => setSortBy("dateAsc")}>Sort by Date Ascending</button>
-      <button onClick={() => setSortBy("dateDesc")}>Sort by Date Descending</button>
+  const updateFavorites = async (updatedFavorites) => {
+    try {
+      // Update the "favorites" table in Supabase with the sorted data
+      const { data, error } = await supabase.from("favorites").upsert(updatedFavorites);
+      if (error) {
+        throw error;
+      }
+      setFavorites(data || []);
+    } catch (error) {
+      console.error("Error updating favorites:", error.message);
+    }
+  };
 
-      {/* Render your favorite items here */}
+  const addToFavorites = async (newFavorite) => {
+    try {
+      // Add a new favorite to the "favorites" table in Supabase
+      const { data, error } = await supabase.from("favorites").upsert([newFavorite]);
+      if (error) {
+        throw error;
+      }
+      setFavorites(data || []);
+    } catch (error) {
+      console.error("Error adding to favorites:", error.message);
+    }
+  };
+
+
+  return (
+    <div className="favorites-container">
+      <h2>My Favorites</h2>
+      <div className="buttons">
+          <button onClick={() => setSortBy("titleAsc")} className="sort-btns">Sort by Title A-Z</button>
+          <button onClick={() => setSortBy("titleDesc")} className="sort-btns">Sort by Title Z-A</button>
+          <button onClick={() => setSortBy("dateAsc")} className="sort-btns">Sort by Date Ascending</button>
+          <button onClick={() => setSortBy("dateDesc")} className="sort-btns">Sort by Date Descending</button>
+      </div>
+      
+
       {favorites.map((favorite) => (
-        <div key={favorite.id}>
-          {/* Render each favorite item */}
+        <div key={favorite.id} className="favorite-item">
+          
           <p>{favorite.showTitle}</p>
           <p>{favorite.dateAdded}</p>
         </div>
